1a9f7f3e7cca371bf7f052316a5cd015d4fa1f11
aaronkik <aaronkikabhai97@gmail.com>
2022-08-09 16:13:18 +0100

    Add extended theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
 	content: [
 		'./pages/**/*.{js,ts,jsx,tsx}',
 		'./components/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				brand: '#38A169',
+			},
+			fontFamily: {
+				sans: ['Inter', ...defaultTheme.fontFamily.sans],
+			},
+		},
 	},
 	plugins: [],
 };
