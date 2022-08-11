beaf0ddef3ebf2f707e16451f5979c582de37b38
Aaron <aaronkikabhai97@gmail.com>
2022-08-11 01:45:53 +0100

    Add intl unit test

---
+++ __tests__/lib/intl.test.ts
@@ -0,0 +1,7 @@
+import { formatValueToGBP } from '../../lib/intl';
+
+describe('formatValueToGBP', () => {
+	it('formats a number to a GBP currency string', () => {
+		expect(formatValueToGBP(1000.5)).toBe('£1,000.50');
+	});
+});
