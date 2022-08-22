d3e2ddc8c824b4d4295dfbb64c9602f093e485dd
Aaron <aaronkikabhai97@gmail.com>
2022-08-23 00:20:16 +0100

    Create form-label component

---
+++ components/form-label.tsx
@@ -0,0 +1,19 @@
+import { DetailedHTMLProps, LabelHTMLAttributes } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+type Props = DetailedHTMLProps<
+	LabelHTMLAttributes<HTMLLabelElement>,
+	HTMLLabelElement
+>;
+
+const FormLabel = ({ className, ...props }: Props) => (
+	<label
+		className={twMerge(
+			'block text-sm font-medium text-neutral-600 dark:text-neutral-400',
+			className
+		)}
+		{...props}
+	/>
+);
+
+export default FormLabel;
